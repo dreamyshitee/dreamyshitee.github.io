cd9eab890d73bf6c2afdabfec4cc1ddd963f3f38
dreamyshitee <aca1811acz@gmail.com>
2025-11-11 08:54:53 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,1 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<body>
 
+<h1> My First PHP page </h1>
+  
+<?php
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+
+
+?>
+
+</body>
+</html>
